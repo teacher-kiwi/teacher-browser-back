702772fb00edbcafe220e9e5abfaec53b9ac66b5
nlom0218 <nlom0218@naver.com>
2021-11-23 22:58:01 +0900

    createUser existUser

--- src/user/createUser/createUser.resolvers.js
+++ src/user/createUser/createUser.resolvers.js
@@ -4,6 +4,13 @@ import User from "../../models/user"
 export default {
   Mutation: {
     createUser: async (_, { email, password }) => {
+      const existUser = await User.findOne({ email }).exec()
+      if (existUser) {
+        return {
+          ok: false,
+          error: "이메일이 존재합니다."
+        }
+      }
       const user = new User({
         email,
         password
