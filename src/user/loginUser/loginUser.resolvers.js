cb85ea9c15a4f8ac02bda8572cb6bb9c0127fb96
nlom0218 <nlom0218@naver.com>
2021-11-28 19:38:11 +0900

    loginUser password confirm

--- src/user/loginUser/loginUser.resolvers.js
+++ src/user/loginUser/loginUser.resolvers.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken"
 import User from "../../models/user"
+import bcrypt from "bcrypt"
 
 export default {
   Query: {
@@ -11,7 +12,14 @@ export default {
           error: "사용자를 찾을 수 없습니다."
         }
       }
-      // password confirm
+
+      const passwordOk = await bcrypt.compare(password, user.password)
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "비밀번호가 틀립니다."
+        }
+      }
 
       const token = await jwt.sign({ email: user.email }, process.env.SECRET_KEY)
       return {
