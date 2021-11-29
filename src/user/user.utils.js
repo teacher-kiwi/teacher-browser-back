e77d3b5639faa75f379de84e05f5bee156a84f52
nlom0218 <nlom0218@naver.com>
2021-11-29 20:10:25 +0900

    user.utils.js

---
+++ src/user/user.utils.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user"
+
+export const getUser = async (token) => {
+  if (!token) {
+    return null
+  }
+  const { email } = await jwt.verify(token, process.env.SECRET_KEY)
+  const user = await User.findOne({ email })
+  if (!user) {
+    return false
+  } else {
+    return user
+  }
+}
+
+export const protectedResovler = (ourResolver) => (root, args, context, info) => {
+  if (!context.loggedInuser) {
+    return {
+      ok: false,
+      error: "로그인이 필요합니다."
+    }
+  }
+  return ourResolver(root, args, context, info)
+}
