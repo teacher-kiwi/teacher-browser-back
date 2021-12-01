0aa91ee1b503e09a04f4b2b9ef670c55c5a2f7b2
nlom0218 <nlom0218@naver.com>
2021-12-01 23:26:41 +0900

    naverLogin

---
+++ src/user/naverLogin/naverLogin.resolvers.js
@@ -0,0 +1,21 @@
+import User from "../../models/user"
+import jwt from "jsonwebtoken"
+
+export default {
+  Mutation: {
+    naverLogin: async (_, { email }) => {
+      const user = await User.findOne({ email })
+      if (!user) {
+        await User.create({ email })
+        return {
+          ok: true
+        }
+      }
+      const token = await jwt.sign({ email }, process.env.SECRET_KEY)
+      return {
+        ok: true,
+        token
+      }
+    }
+  }
+}
