4d25f77f07dbf10de74449b2eddb5d0dbc5e5c8e
nlom0218 <nlom0218@naver.com>
2021-12-10 21:41:30 +0900

    create Student Schema

---
+++ src/models/student.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const { Schema } = mongoose
+
+const studentSchema = new Schema({
+  teacherEmail: {
+    type: String,
+    required: true
+  }
+})
+
+const Student = mongoose.model("Student", studentSchema)
+
+export default Student
--- src/models/user.js
+++ src/models/user.js
@@ -21,6 +21,9 @@ const userSchema = new Schema({
   },
   schoolAdress: {
     type: String
+  },
+  studentsNum: {
+    type: Number
   }
 })
 
