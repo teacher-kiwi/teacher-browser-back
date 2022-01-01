f6e43e005121ef4b832772228a0c729473d1d924
teacher-kiwi <kwi07@hanmail.net>
2022-01-01 21:40:50 +0900

    editStudentList 아이콘 삭제 기능 추가

--- src/studentList/editStudentList/editStudentList.resolvers.js
+++ src/studentList/editStudentList/editStudentList.resolvers.js
@@ -24,6 +24,7 @@ export default {
       if (listIcon) {
         await StudentList.updateOne({ _id: listId }, { listIcon });
       }
+      if (listIcon === "delete") await StudentList.updateOne({ _id: listId }, { listIcon: null });
 
       return { ok: true };
     }),
