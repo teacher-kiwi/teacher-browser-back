79083b7708fe33b41ac65abe49532edd220b6946
teacher-kiwi <kwi07@hanmail.net>
2022-01-02 22:59:59 +0900

    seeAllStudent 한 학생 보기 기능 추가

--- src/student/seeAllStudent/seeAllStudent.resolvers.js
+++ src/student/seeAllStudent/seeAllStudent.resolvers.js
@@ -3,8 +3,11 @@ import { protectedQueryResovler } from "../../user/user.utils";
 
 export default {
   Query: {
-    seeAllStudent: protectedQueryResovler(async (_, __, { loggedInUser }) => {
-      return await Student.find({ teacherEmail: loggedInUser.email }).sort({ _id: 1 });
+    seeAllStudent: protectedQueryResovler(async (_, { studentId }, { loggedInUser }) => {
+      // studentId 값이 있으면 한 학생 보기
+      if (studentId) return await Student.find({ _id: studentId, teacherEmail: loggedInUser.email });
+      // studentId 값이 없으면 모든 학생 보기
+      else return await Student.find({ teacherEmail: loggedInUser.email }).sort({ _id: 1 });
     }),
   },
 };
--- src/student/seeAllStudent/seeAllStudent.typeDefs.js
+++ src/student/seeAllStudent/seeAllStudent.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    seeAllStudent: [Student]
+    seeAllStudent(studentId: ID): [Student]
   }
-`
+`;
