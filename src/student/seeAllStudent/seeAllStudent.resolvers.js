33ba22a79f7d1cc7d28b7a25eef3821babaed9ec
teacher-kiwi <kwi07@hanmail.net>
2022-01-06 23:56:34 +0900

    seeAllStudent 알러지 정보로 학생 검색 기능 추가

--- src/student/seeAllStudent/seeAllStudent.resolvers.js
+++ src/student/seeAllStudent/seeAllStudent.resolvers.js
@@ -3,10 +3,12 @@ import { protectedQueryResovler } from "../../user/user.utils";
 
 export default {
   Query: {
-    seeAllStudent: protectedQueryResovler(async (_, { studentId }, { loggedInUser }) => {
-      // studentId 값이 있으면 한 학생 보기
-      if (studentId) return await Student.find({ _id: studentId, teacherEmail: loggedInUser.email });
-      // studentId 값이 없으면 모든 학생 보기
+    seeAllStudent: protectedQueryResovler(async (_, { studentId, allergy }, { loggedInUser }) => {
+      // allergy 값이 있으면 allergy 모든 학생 보기
+      if (allergy) return await Student.find({ teacherEmail: loggedInUser.email, allergy });
+      // studentId 값만 있으면 한 학생 보기
+      if (studentId) return await Student.findOne({ _id: studentId, teacherEmail: loggedInUser.email });
+      // 아무 값도 없으면 모든 학생 보기
       else return await Student.find({ teacherEmail: loggedInUser.email }).sort({ _id: 1 });
     }),
   },
--- src/student/seeAllStudent/seeAllStudent.typeDefs.js
+++ src/student/seeAllStudent/seeAllStudent.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    seeAllStudent(studentId: ID): [Student]
+    seeAllStudent(studentId: ID, allergy: Int): [Student]
   }
 `;
