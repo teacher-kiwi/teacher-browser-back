9c8caed58e26cb922ce4d85c0f462929e713f0be
teacher-kiwi <kwi07@hanmail.net>
2022-01-09 18:30:54 +0900

    학생 리스트 이름 수정 시 앞뒤 빈칸 제거

--- src/studentList/editStudentList/editStudentList.resolvers.js
+++ src/studentList/editStudentList/editStudentList.resolvers.js
@@ -6,11 +6,11 @@ export default {
     editStudentList: protectedMutationResovler(async (_, { teacherEmail, listId, listName, listOrder, listIcon }, { loggedInUser }) => {
       //
       // 바꿀 이름이 이미 있는지 검사
-      const existStudentList = await StudentList.findOne({ teacherEmail, listName });
+      const existStudentList = await StudentList.findOne({ teacherEmail, listName: listName.trim() });
       if (existStudentList) return { ok: false, error: "같은 이름의 리스트가 존재합니다." };
       //
       // 바꿀 이름이 없다면 수정
-      await StudentList.updateOne({ _id: listId }, { listName, listOrder, listIcon });
+      await StudentList.updateOne({ _id: listId }, { listName: listName.trim(), listOrder, listIcon });
       //
       // listIcon 값이 delete이면 null로 저장
       if (listIcon === "delete") await StudentList.updateOne({ _id: listId }, { listIcon: null });
