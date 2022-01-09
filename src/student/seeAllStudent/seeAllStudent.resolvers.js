c374375193f540675aa49788596b0e446744e65d
teacher-kiwi <kwi07@hanmail.net>
2022-01-09 22:41:31 +0900

    tag 값으로 학생 검색하기 기능 추가

--- src/student/seeAllStudent/seeAllStudent.resolvers.js
+++ src/student/seeAllStudent/seeAllStudent.resolvers.js
@@ -4,6 +4,9 @@ import { protectedQueryResovler } from "../../user/user.utils";
 export default {
   Query: {
     seeAllStudent: protectedQueryResovler(async (_, { studentId, allergy }, { loggedInUser }) => {
+      // tag 값이 있으면 tag가 있는 학생들 보기
+      if (tag) return await Student.find({ teacherEmail: loggedInUser.email, tag: { $all: tag } });
+
       // allergy 값이 있으면 allergy 모든 학생 보기
       if (allergy) return await Student.find({ teacherEmail: loggedInUser.email, allergy }).sort({ studentNumber: 1 });
       // studentId 값만 있으면 한 학생 보기
