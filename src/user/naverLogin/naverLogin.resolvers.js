b28bdc8f61b2682932026b4b49b277f44494c3a2
teacher-kiwi <kwi07@hanmail.net>
2022-01-11 11:46:10 +0900

    계정 생성 시 기본 태그 추가

--- src/user/naverLogin/naverLogin.resolvers.js
+++ src/user/naverLogin/naverLogin.resolvers.js
@@ -5,7 +5,7 @@ export default {
   Mutation: {
     naverLogin: async (_, { email }) => {
       const user = await User.findOne({ email });
-      if (!user) await User.create({ email });
+      if (!user) await User.create({ email, tag: ["남학생", "여학생", "홀수", "짝수"] });
       const token = await jwt.sign({ email }, process.env.SECRET_KEY);
       return { ok: true, token };
     },
