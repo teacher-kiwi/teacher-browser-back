3c14eeaa7df0f82aa9d90e18095db714b1344ffd
teacher-kiwi <kwi07@hanmail.net>
2022-01-11 13:18:59 +0900

    학생보기에 정렬 기능 추가

--- src/student/seeAllStudent/seeAllStudent.resolvers.js
+++ src/student/seeAllStudent/seeAllStudent.resolvers.js
@@ -3,16 +3,27 @@ import { protectedQueryResovler } from "../../user/user.utils";
 
 export default {
   Query: {
-    seeAllStudent: protectedQueryResovler(async (_, { studentId, allergy, tag }, { loggedInUser }) => {
+    seeAllStudent: protectedQueryResovler(async (_, { studentId, allergy, tag, sort }, { loggedInUser }) => {
+      let sortValue;
+      sort === "name"
+        ? // sort 값이 "name"일 경우 studentName으로 정렬
+          (sortValue = { studentName: 1 })
+        : sort === "num"
+        ? // sort 값이 "num"일 경우 studentNumber로 정렬
+          (sortValue = { studentNumber: 1 })
+        : // sort 값이 없거나 "name" 이나 "num" 값이 아닐 경우 id(생성일)순으로 보기
+          (sortValue = { _id: 1 });
+
       // tag 값이 있으면 tag가 있는 학생들 보기
       if (tag) return await Student.find({ teacherEmail: loggedInUser.email, tag: { $all: tag } });
 
       // allergy 값이 있으면 allergy 모든 학생 보기
-      if (allergy) return await Student.find({ teacherEmail: loggedInUser.email, allergy }).sort({ studentNumber: 1 });
+      if (allergy) return await Student.find({ teacherEmail: loggedInUser.email, allergy }).sort(sortValue).collation({ locale: "ko", numericOrdering: true });
+
       // studentId 값만 있으면 한 학생 보기
       if (studentId) return await Student.find({ _id: studentId, teacherEmail: loggedInUser.email });
       // 아무 값도 없으면 모든 학생 보기
-      else return await Student.find({ teacherEmail: loggedInUser.email }).sort({ studentNumber: 1 });
+      else return await Student.find({ teacherEmail: loggedInUser.email }).sort(sortValue).collation({ locale: "ko", numericOrdering: true });
     }),
   },
 };
