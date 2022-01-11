fe1fe55f22e8d1f54798a54af84babbb67760407
teacher-kiwi <kwi07@hanmail.net>
2022-01-11 15:30:40 +0900

    deleteUser 유저 정보 삭제 시 다른 모든 정보도 함께 삭제

--- src/user/deleteUser/deleteUser.resolvers.js
+++ src/user/deleteUser/deleteUser.resolvers.js
@@ -1,10 +1,18 @@
 import User from "../../models/user";
+import Student from "../../models/student";
+import StudentList from "../../models/studentList";
+import TodoList from "../../models/toDoList";
+
+import { protectedMutationResovler } from "../../user/user.utils";
 
 export default {
   Mutation: {
-    deleteUser: async (_, { email }) => {
-      await User.findOneAndDelete({ email });
+    deleteUser: protectedMutationResovler(async (_, { teacherEmail }, { loggedInUser }) => {
+      await User.findOneAndDelete({ email: teacherEmail });
+      await Student.deleteMany({ teacherEmail });
+      await StudentList.deleteMany({ teacherEmail });
+      await TodoList.deleteMany({ userEmail: teacherEmail });
       return { ok: true };
-    },
+    }),
   },
 };
--- src/user/deleteUser/deleteUser.typeDefs.js
+++ src/user/deleteUser/deleteUser.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation {
-    deleteUser(email: String!): mutationResult
+    deleteUser(teacherEmail: String!): mutationResult
   }
-`
+`;
