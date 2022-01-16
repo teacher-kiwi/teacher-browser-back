b9e1ea35487e33c9db7e3176a697200eaa6ae9f1
teacher-kiwi <kwi07@hanmail.net>
2022-01-17 01:20:09 +0900

    deleteAllStudent 휴지통에 있는 학생만 전부 삭제로 수정

--- src/student/deleteAllStudent/deleteAllStudent.resolvers.js
+++ src/student/deleteAllStudent/deleteAllStudent.resolvers.js
@@ -5,8 +5,16 @@ import { protectedMutationResovler } from "../../user/user.utils";
 export default {
   Mutation: {
     deleteAllStudent: protectedMutationResovler(async (_, { teacherEmail }, { loggedInUser }) => {
-      await Student.deleteMany({ teacherEmail });
-      await StudentList.updateMany({ teacherEmail }, { studentId: [] });
+      //
+      // 휴지통에 있는 학생을 찾아서 id 값만 배열로 만들기
+      const students = await Student.find({ teacherEmail, trash: true });
+      const ids = students.map((x) => x._id);
+      //
+      // 학생 리스트 전체를 뒤져서 휴지통에 있는 학생 id를 삭제하기
+      await StudentList.updateMany({ teacherEmail }, { $pull: { studentId: { $in: ids } } });
+      //
+      // 마지막으로 휴지통에 있는 학생 다 지우기
+      await Student.deleteMany({ teacherEmail, trash: true });
       return { ok: true };
     }),
   },
