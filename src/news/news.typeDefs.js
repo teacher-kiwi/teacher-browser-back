215ae0feeded50445bab8e7b0d827cc79691dd70
nlom0218 <nlom0218@naver.com>
2022-01-22 21:01:11 +0900

    news

---
+++ src/news/news.typeDefs.js
@@ -0,0 +1,15 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type News {
+    title: String,
+    originallink: String,
+    link: String,
+    description: String,
+    pubDate: String
+  }
+
+  type Query {
+    getNews(search: String!, start: Int!, sort: String!): [News]
+  }
+`;
