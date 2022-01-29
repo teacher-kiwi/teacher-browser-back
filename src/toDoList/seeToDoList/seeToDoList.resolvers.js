5c3050af752ffb1e652be4adb11413eb4b4e3212
nlom0218 <nlom0218@naver.com>
2022-01-29 17:24:39 +0900

    seeToDoList

--- src/toDoList/seeToDoList/seeToDoList.resolvers.js
+++ src/toDoList/seeToDoList/seeToDoList.resolvers.js
@@ -3,8 +3,8 @@ import { protectedQueryResovler } from "../../user/user.utils";
 
 export default {
   Query: {
-    seeToDoList: protectedQueryResovler(async (_, __, { loggedInUser }) => {
-      return await ToDoList.find({ userEmail: loggedInUser.email });
+    seeToDoList: protectedQueryResovler(async (_, { isComplete }, { loggedInUser }) => {
+      return await ToDoList.find({ userEmail: loggedInUser.email, isComplete });
     }),
   },
 };
