34e340625516485418042b7d28b7431dd648c3c1
nlom0218 <nlom0218@naver.com>
2022-02-05 16:17:28 +0900

    editPageLinkMemo

---
+++ src/user/editPageLinkMemo/editPageLinkMemo.resolvers.js
@@ -0,0 +1,22 @@
+import User from "../../models/user";
+import { protectedMutationResovler } from "../user.utils";
+
+export default {
+  Mutation: {
+    editPageLinkMemo: protectedMutationResovler(async (_, { userEmail, memo, pageTitle }) => {
+      const user = await User.findOne({ email: userEmail })
+      const userLink = user.link
+      const newUserLink = userLink.map((item) => {
+        if (item.siteName === pageTitle) {
+          return { siteName: pageTitle, memo, _id: item._id }
+        } else {
+          return item
+        }
+      })
+      await User.updateOne({ email: userEmail }, { link: newUserLink })
+      return {
+        ok: true
+      }
+    })
+  }
+}
---
+++ src/user/editPageLinkMemo/editPageLinkMemo.typeDefs.js
@@ -0,0 +1,7 @@
+import { gql } from "apollo-server-core"
+
+export default gql` 
+  type Mutation {
+    editPageLinkMemo(userEmail: String!, memo: String!, pageTitle: String!): mutationResult
+  }
+`
