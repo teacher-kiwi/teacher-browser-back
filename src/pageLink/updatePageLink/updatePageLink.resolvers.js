b3221942e86eab584faf19156d87cd3000388bde
nlom0218 <nlom0218@naver.com>
2022-02-05 17:21:17 +0900

    updatePageLink updateAt

--- src/pageLink/updatePageLink/updatePageLink.resolvers.js
+++ src/pageLink/updatePageLink/updatePageLink.resolvers.js
@@ -3,7 +3,7 @@ import PageLink from "../../models/pageLink"
 export default {
   Mutation: {
     updatePageLink: async (_, { pageTitle, pageDescription, folder }) => {
-      await PageLink.updateOne({ pageTitle }, { pageDescription, folder })
+      await PageLink.updateOne({ pageTitle }, { pageDescription, folder, updateAt: new Date() })
       return {
         ok: true
       }
