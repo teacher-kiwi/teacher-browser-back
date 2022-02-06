876a9291d9d970aaeda157d18762c3edb2f0568a
nlom0218 <nlom0218@naver.com>
2022-02-06 21:56:55 +0900

    editToDoList

--- src/toDoList/editToDoList/editToDoList.resolvers.js
+++ src/toDoList/editToDoList/editToDoList.resolvers.js
@@ -4,7 +4,16 @@ import { protectedMutationResovler } from "../../user/user.utils";
 export default {
   Mutation: {
     editToDoList: protectedMutationResovler(async (_, { _id, userEmail, toDo, star, startDate, endDate, contents }, { loggedInUser }) => {
-      await ToDoList.updateOne({ _id, userEmail }, { toDo, contents, $set: { toDo, star, startDate, endDate } });
+      if (!startDate) {
+        await ToDoList.updateOne({
+          _id, userEmail
+        },
+          {
+            startDate: null,
+            endDate: null
+          });
+      }
+      await ToDoList.updateOne({ _id, userEmail }, { toDo, contents, star, startDate, endDate });
       return { ok: true };
     }),
   },
