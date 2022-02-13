99d685d41bcf938b1a1a51a9d8c0546a34374afb
teacher-kiwi <kwi07@hanmail.net>
2022-02-13 23:33:47 +0900

    dotenv 삭제

--- src/server.js
+++ src/server.js
@@ -1,20 +1,19 @@
-require("dotenv").config()
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core"
-import { ApolloServer } from "apollo-server-express"
-import express from "express"
-import http from "http"
-import logger from "morgan"
-import { resolvers, typeDefs } from "./schema"
-import dbConnect from "./models/index"
-import { getUser } from "./user/user.utils"
-dbConnect()
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import http from "http";
+import logger from "morgan";
+import { resolvers, typeDefs } from "./schema";
+import dbConnect from "./models/index";
+import { getUser } from "./user/user.utils";
+dbConnect();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 // https://www.apollographql.com/docs/apollo-server/integrations/middleware/#apollo-server-express
 async function startApolloServer(typeDefs, resolvers) {
-  const app = express()
-  const httpServer = http.createServer(app)
+  const app = express();
+  const httpServer = http.createServer(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -23,17 +22,17 @@ async function startApolloServer(typeDefs, resolvers) {
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     context: async ({ req }) => {
       return {
-        loggedInUser: await getUser(req.headers.token)
-      }
-    }
-  })
-  await server.start()
+        loggedInUser: await getUser(req.headers.token),
+      };
+    },
+  });
+  await server.start();
   // 필요한 미들웨어 작성
-  app.use(logger("tiny"))
+  app.use(logger("tiny"));
 
-  server.applyMiddleware({ app })
-  await new Promise(r => app.listen({ port: PORT }, r))
+  server.applyMiddleware({ app });
+  await new Promise((r) => app.listen({ port: PORT }, r));
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 }
 
-startApolloServer(typeDefs, resolvers)
+startApolloServer(typeDefs, resolvers);
