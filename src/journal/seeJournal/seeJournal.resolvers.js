61c90f2d1ea171dcc9e8d9bd31dd2059f9c6523d
nlom0218 <nlom0218@naver.com>
2022-02-15 21:14:30 +0900

    seeJournal journalId

--- src/journal/seeJournal/seeJournal.resolvers.js
+++ src/journal/seeJournal/seeJournal.resolvers.js
@@ -3,9 +3,12 @@ import { protectedQueryResovler } from "../../user/user.utils";
 
 export default {
   Query: {
-    seeJournal: protectedQueryResovler(async (_, { teacherEmail, date, studentId }) => {
+    seeJournal: protectedQueryResovler(async (_, { teacherEmail, date, studentId, journalId }) => {
       if (date) {
         return await Journal.find({ teacherEmail, date: new Date(date).setHours(0, 0, 0, 0) }).sort({ _id: 1 })
+      }
+      if (journalId) {
+        return await Journal.find({ teacherEmail, _id: journalId })
       }
     })
   }
--- src/journal/seeJournal/seeJournal.typeDefs.js
+++ src/journal/seeJournal/seeJournal.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    seeJournal(teacherEmail: String!, date: String, studentId: ID): [Journal]
+    seeJournal(teacherEmail: String!, date: String, studentId: ID, journalId: String): [Journal]
   }
 `
