71b46aa7462bedba2a6d02a95c3c10cdada4d15a
teacher-kiwi <kwi07@hanmail.net>
2022-02-20 20:35:59 +0900

    구글 로그인 기능 보완

--- src/user/googleLogin/googleLogin.resolvers.js
+++ src/user/googleLogin/googleLogin.resolvers.js
@@ -1,13 +1,20 @@
 import User from "../../models/user";
 import jwt from "jsonwebtoken";
+import axios from "axios";
 
 export default {
   Mutation: {
-    googleLogin: async (_, { email }) => {
-      const user = await User.findOne({ email });
-      if (!user) await User.create({ email });
-      const token = await jwt.sign({ email }, process.env.SECRET_KEY);
-      return { ok: true, token };
+    googleLogin: async (_, { access_token }) => {
+      return axios(`https://www.googleapis.com/oauth2/v2/userinfo?access_token=${access_token}`).then(async (data) => {
+        if (data.data) {
+          const user = await User.findOne({ email: data.data.email });
+          if (!user) await User.create({ email: data.data.email });
+          const token = await jwt.sign({ email: data.data.email }, process.env.SECRET_KEY);
+          return { ok: true, token };
+        } else if (data.error) {
+          return { ok: false, error: data.error.status };
+        }
+      });
     },
   },
 };
--- src/user/googleLogin/googleLogin.typeDefs.js
+++ src/user/googleLogin/googleLogin.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation {
-    googleLogin(email: String!): loginResult
+    googleLogin(access_token: String!): loginResult
   }
 `;
