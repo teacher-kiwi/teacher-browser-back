b9f6d9804db0e1f61b75530eb54788da8787448b
teacher-kiwi <kwi07@hanmail.net>
2022-02-20 20:35:20 +0900

    시간표 기능 수정(day, time을 index로)

--- src/timetable/getTimetableData/getTimetableData.resolvers.js
+++ src/timetable/getTimetableData/getTimetableData.resolvers.js
@@ -5,11 +5,27 @@ export default {
   Query: {
     getTimetableData: protectedQueryResovler(async (_, { day }, { loggedInUser }) => {
       if (day) {
-        const timetableData = await TimetableData.find({ teacherEmail: loggedInUser.email, day }).sort({ time: 1 });
-        return timetableData;
+        const timetableData = await TimetableData.find({ teacherEmail: loggedInUser.email, day }).sort({ index: 1 });
+        if (timetableData.length !== 0) {
+          return timetableData;
+        } else {
+          for (let i = 0; i < 30; i++) {
+            await TimetableData.create({ teacherEmail: loggedInUser.email, index: i, day: i % 5 });
+          }
+          const newTimetableData = await TimetableData.find({ teacherEmail: loggedInUser.email, day }).sort({ index: 1 });
+          return newTimetableData;
+        }
       } else {
-        const timetableData = await TimetableData.find({ teacherEmail: loggedInUser.email }).sort({ day: 1, time: 1 });
-        return timetableData;
+        const timetableData = await TimetableData.find({ teacherEmail: loggedInUser.email }).sort({ index: 1 });
+        if (timetableData.length !== 0) {
+          return timetableData;
+        } else {
+          for (let i = 0; i < 30; i++) {
+            await TimetableData.create({ teacherEmail: loggedInUser.email, index: i, day: i % 5 });
+          }
+          const newTimetableData = await TimetableData.find({ teacherEmail: loggedInUser.email }).sort({ index: 1 });
+          return newTimetableData;
+        }
       }
     }),
   },
--- src/timetable/timetableData.typeDefs.js
+++ src/timetable/timetableData.typeDefs.js
@@ -4,8 +4,8 @@ export default gql`
   type TimetableData {
     _id: ID
     teacherEmail: String
+    index: Int
     day: Int
-    time: Int
     subName: String
     color: String
     memo: String
