997c03fc3653f7c6ede956a7f7c09eaabfbea39b
nlom0218 <nlom0218@naver.com>
2022-03-31 22:24:45 +0900

    seeAttendance month

--- src/attendance/seeAttendance/seeAttendance.typeDefs.js
+++ src/attendance/seeAttendance/seeAttendance.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    seeAttendance(date: Float, studentId: String, attendId: String): [Attendance]
+    seeAttendance(date: Float, studentId: String, attendId: String, month: Int): [Attendance]
   }
 `
