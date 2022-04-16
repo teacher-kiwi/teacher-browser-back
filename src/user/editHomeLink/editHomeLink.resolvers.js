53326d892b202318a2da92e256957bba9df5ad77
nlom0218 <nlom0218@naver.com>
2022-04-16 21:41:14 +0900

    del console in editHomeLink

--- src/user/editHomeLink/editHomeLink.resolvers.js
+++ src/user/editHomeLink/editHomeLink.resolvers.js
@@ -7,14 +7,12 @@ export default {
       async (_, { userEmail, ID, title, link }) => {
         const user = await User.findOne({ email: userEmail });
         const userHomeLinks = user.homeLinks;
-        console.log(userHomeLinks);
         const targetIndex = userHomeLinks.findIndex((item) => item.ID === ID);
         const newHomeLinks = [
           ...userHomeLinks.slice(0, targetIndex),
           { title, link, ID },
           ...userHomeLinks.slice(targetIndex + 1),
         ];
-        console.log(newHomeLinks);
         await User.updateOne({ email: userEmail }, { homeLinks: newHomeLinks });
         return {
           ok: true,
