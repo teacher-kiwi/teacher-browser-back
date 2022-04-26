3e46315d1d6ab900a2afb75cb7fcf2ee7087a22b
nlom0218 <nlom0218@naver.com>
2022-04-26 21:07:04 +0900

    familMonth models

--- src/models/journal.js
+++ src/models/journal.js
@@ -6,7 +6,7 @@ const journalSchema = new Schema({
   ownerId: { type: String, required: true },
   date: { type: Date, required: true },
   text: { type: String, trim: true, required: true },
-  teacherEmail: { type: String, required: true }
+  teacherEmail: { type: String, required: true },
 });
 
 const Journal = mongoose.model("Journal", journalSchema);
