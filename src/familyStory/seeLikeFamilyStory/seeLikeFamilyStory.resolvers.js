27c81121dff69afe208b93c2e8bff4d22bd0c44f
nlom0218 <nlom0218@naver.com>
2022-04-27 23:24:27 +0900

    seeLikeFamilyStory

---
+++ src/familyStory/seeLikeFamilyStory/seeLikeFamilyStory.resolvers.js
@@ -0,0 +1,10 @@
+import FamilyStoryLike from "../../models/familyStoryLike";
+import { protectedMutationResovler } from "../../user/user.utils";
+
+export default {
+  Query: {
+    seeLikeFamilyStory: protectedMutationResovler(async (_, { userEmail }) => {
+      return await FamilyStoryLike.find({ userEmail });
+    }),
+  },
+};
