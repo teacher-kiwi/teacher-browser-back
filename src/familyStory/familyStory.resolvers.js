bced0cc2b373d6eba47a3f471a457666481f9536
nlom0218 <nlom0218@naver.com>
2022-04-27 22:40:49 +0900

    familStory isLiked

--- src/familyStory/familyStory.resolvers.js
+++ src/familyStory/familyStory.resolvers.js
@@ -3,7 +3,21 @@ import FamilyStoryLike from "../models/familyStoryLike";
 export default {
   FamilyStory: {
     likeNum: async ({ _id }) => {
-      return FamilyStoryLike.count({ familyStoryId: _id });
+      return await FamilyStoryLike.count({ familyStoryId: _id });
+    },
+    isLiked: async ({ _id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      const like = await FamilyStoryLike.findOne({
+        familyStoryId: _id,
+        userEmail: loggedInUser.email,
+      });
+      if (like) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
 };
--- src/familyStory/familyStory.typeDefs.js
+++ src/familyStory/familyStory.typeDefs.js
@@ -13,6 +13,7 @@ export default gql`
     contents: String!
     #
     likeNum: Int!
+    isLiked: Boolean
   }
 
   type FamilyStoryLike {
