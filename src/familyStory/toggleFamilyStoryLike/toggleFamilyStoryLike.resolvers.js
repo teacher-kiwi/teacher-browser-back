a8897c3b7eab0c88ef04103c0df342275b880ff9
nlom0218 <nlom0218@naver.com>
2022-04-27 22:52:04 +0900

    toggleFamilyStoryLike

---
+++ src/familyStory/toggleFamilyStoryLike/toggleFamilyStoryLike.resolvers.js
@@ -0,0 +1,29 @@
+import FamilyStoryLike from "../../models/familyStoryLike";
+import { protectedMutationResovler } from "../../user/user.utils";
+
+export default {
+  Mutation: {
+    toggleFamilyStoryLike: protectedMutationResovler(
+      async (_, { userEmail, familyStoryId }) => {
+        const like = await FamilyStoryLike.findOne({
+          userEmail,
+          familyStoryId,
+        });
+        if (like) {
+          await FamilyStoryLike.deleteOne({
+            userEmail,
+            familyStoryId,
+          });
+        } else {
+          await FamilyStoryLike.create({
+            userEmail,
+            familyStoryId,
+          });
+        }
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
