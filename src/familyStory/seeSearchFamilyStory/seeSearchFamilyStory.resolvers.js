fb31c8b4f3c339c75cc0342530d64dfccbc727a6
nlom0218 <nlom0218@naver.com>
2022-04-29 00:13:13 +0900

    seeSearchFamilyStory page

--- src/familyStory/seeSearchFamilyStory/seeSearchFamilyStory.resolvers.js
+++ src/familyStory/seeSearchFamilyStory/seeSearchFamilyStory.resolvers.js
@@ -2,10 +2,13 @@ import FamilyStory from "../../models/familyStory";
 
 export default {
   Query: {
-    seeSearchFamilyStory: async (_, { tag }) => {
+    seeSearchFamilyStory: async (_, { tag, page }) => {
       return await FamilyStory.find({
         tag: { $in: tag },
-      }).sort({ createdAt: -1 });
+      })
+        .skip((page - 1) * 12)
+        .limit(12)
+        .sort({ createdAt: -1 });
     },
   },
 };
--- src/familyStory/seeSearchFamilyStory/seeSearchFamilyStory.typeDefs.js
+++ src/familyStory/seeSearchFamilyStory/seeSearchFamilyStory.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    seeSearchFamilyStory(tag: String!): [FamilyStory]
+    seeSearchFamilyStory(tag: String!, page: Int!): [FamilyStory]
   }
 `;
