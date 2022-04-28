d8401734c49e0ba82bc42e82bdabcfeb3282e69f
nlom0218 <nlom0218@naver.com>
2022-04-29 00:09:27 +0900

    seeLikeFamilyStory page

--- src/familyStory/seeLikeFamilyStory/seeLikeFamilyStory.resolvers.js
+++ src/familyStory/seeLikeFamilyStory/seeLikeFamilyStory.resolvers.js
@@ -3,10 +3,15 @@ import { protectedMutationResovler } from "../../user/user.utils";
 
 export default {
   Query: {
-    seeLikeFamilyStory: protectedMutationResovler(async (_, { userEmail }) => {
-      return await FamilyStoryLike.find({ userEmail }).sort({
-        familyStoryCreatedAt: -1,
-      });
-    }),
+    seeLikeFamilyStory: protectedMutationResovler(
+      async (_, { userEmail, page }) => {
+        return await FamilyStoryLike.find({ userEmail })
+          .skip((page - 1) * 12)
+          .limit(12)
+          .sort({
+            familyStoryCreatedAt: -1,
+          });
+      }
+    ),
   },
 };
--- src/familyStory/seeLikeFamilyStory/seeLikeFamilyStory.typeDefs.js
+++ src/familyStory/seeLikeFamilyStory/seeLikeFamilyStory.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    seeLikeFamilyStory(userEmail: String!): [FamilyStoryLike]
+    seeLikeFamilyStory(userEmail: String!, page: Int!): [FamilyStoryLike]
   }
 `;
