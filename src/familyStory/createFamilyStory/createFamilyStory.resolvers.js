af597a2380fa6941ed10949f15df133598e915b9
nlom0218 <nlom0218@naver.com>
2022-05-01 12:29:31 +0900

    createFamilyStory user error msg

--- src/familyStory/createFamilyStory/createFamilyStory.resolvers.js
+++ src/familyStory/createFamilyStory/createFamilyStory.resolvers.js
@@ -1,11 +1,20 @@
 import FamilyStory from "../../models/familyStory";
+import User from "../../models/user";
 
 export default {
   Mutation: {
     createFamilyStory: async (
       _,
-      { userEmail, url, title, bgColor, videoType, tag, createdAt, contents }
+      { userEmail, url, title, bgColor, videoType, tag, createdAt, contents },
+      { loggedInUser }
     ) => {
+      const user = await User.findOne({ email: userEmail });
+      if (!loggedInUser && user) {
+        return {
+          ok: false,
+          error: "사용할 수 없는 닉네임입니다. 다른 닉네임을 입력하세요.😂",
+        };
+      }
       const newFamilyStory = await FamilyStory.create({
         userEmail,
         url,
