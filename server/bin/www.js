4c34f029b4a9bd85ef0573aa2395ca1dacf28825
teacher-kiwi <kwi07@hanmail.net>
2022-08-31 16:52:32 +0900

    테스트 수정(로컬 db 사용)

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,4 +1,6 @@
 const server = require("../server");
 const PORT = process.env.PORT;
 
+require("../src/config/db");
+
 server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
