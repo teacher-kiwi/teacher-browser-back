8393e7471f8a189aadfa128b1ebb2e5c460351a4
teacher-kiwi <kwi07@hanmail.net>
2022-10-02 16:41:01 +0900

    createAttendance mutation 수정

--- server/src/typeDefs-resolvers/attendance/attendance.js
+++ server/src/typeDefs-resolvers/attendance/attendance.js
@@ -21,17 +21,40 @@ const resolver = {
   },
 
   Mutation: {
-    createAttendance: protectedMutation(async (_, { userEmail, studentId, type, date, contents, month }) => {
-      await Attendance.create({
-        userEmail,
-        studentId,
-        type,
-        date,
-        month,
-        ...(contents && { contents }),
-      });
+    createAttendance: protectedMutation(async (_, { userEmail, studentId, type, contents, dateMonthArr }) => {
+      try {
+        for (const studentIndex in studentId) {
+          console.log(studentId[studentIndex]);
+          for (const dateIndex in dateMonthArr) {
+            console.log(dateMonthArr[dateIndex]);
+            await Attendance.create({
+              userEmail,
+              studentId: studentId[studentIndex],
+              type,
+              ...(contents && { contents }),
+              date: dateMonthArr[dateIndex].date,
+              month: dateMonthArr[dateIndex].month,
+            });
+          }
+        }
+      } catch (err) {
+        throw err.massage;
+      }
+
       return { ok: true };
     }),
+
+    // createAttendance: protectedMutation(async (_, { userEmail, studentId, type, date, contents, month }) => {
+    //   await Attendance.create({
+    //     userEmail,
+    //     studentId,
+    //     type,
+    //     date,
+    //     month,
+    //     ...(contents && { contents }),
+    //   });
+    //   return { ok: true };
+    // }),
 
     createManyAttendance: protectedMutation(async (_, { userEmail, studentId, type, contents, dateMonthArr }) => {
       for (let i = 0; i < dateMonthArr.length; i++) {
